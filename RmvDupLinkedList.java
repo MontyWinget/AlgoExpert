fcf88b4a6921f0ff6bd4509044cfc299ca6c947a
Monty Winget <monty.winget@gmail.com>
2021-11-30 12:48:39 +0530

    Remove duplicate nodes in a sorted Linked List.

---
+++ RmvDupLinkedList.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Program {
+  // This is an input class. Do not edit.
+  public static class LinkedList {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+	O(n) Time | O(1) space
+  public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+    
+		LinkedList currentNode = linkedList;
+		
+		while(currentNode != null){
+			LinkedList nextDistinctNode = currentNode.next;
+			while(nextDistinctNode != null && nextDistinctNode.value == currentNode.value){
+				nextDistinctNode = nextDistinctNode.next;
+			}
+			currentNode.next = nextDistinctNode;
+			currentNode = nextDistinctNode;
+		}
+		
+    return linkedList;
+  }
+}
